114a97986d5c0f328aa0015bf5e68c50ad4ec4c0
linjincheng <ljc@choicesoft.com.cn>
2018-09-30 16:30:12 +0800

    调整集群方式启动的代码

--- src/main/java/com/cohen/TopologyCreator.java
+++ src/main/java/com/cohen/TopologyCreator.java
@@ -2,6 +2,7 @@ package com.cohen;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
 import backtype.storm.tuple.Fields;
 import com.cohen.function.BillPredealFunction;
 import com.cohen.function.NamtAggregator;
@@ -19,16 +20,10 @@ import java.util.List;
  * @date 2018/9/29 10:45
  */
 public class TopologyCreator {
+    private static final String TOPOLOGY_NAME = "tridentTopology";
+
     public static void main(String[] args) throws Exception {
         boolean local = true;
-        if (local) {
-            local();
-        } else {
-            remote();
-        }
-    }
-
-    private static void local() throws InterruptedException {
         TridentTopology tridentTopology = new TridentTopology();
         Stream kafkaStream = tridentTopology.newStream("kafka-stream", new KafkaSpout());
         Stream groupStream = kafkaStream.each(new Fields("batchId", "billJson"), new BillPredealFunction(), new Fields("bill"));
@@ -38,40 +33,17 @@ public class TopologyCreator {
         tridentTopology.merge(aggregateStreams)
                 .partitionBy(new Fields("key"))
                 .partitionAggregate(new Fields("key", "value"), new SumAggregator(), new Fields("sum"));
-        LocalCluster cluster = new LocalCluster();
         Config conf = new Config();
         conf.setNumWorkers(1);
         //设置storm的超时时间大于设置的超时时间
         conf.setMessageTimeoutSecs(70);
         conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 2);
-        cluster.submitTopology("tridentTopology", conf, tridentTopology.build());
-    }
-
-    private static void remote() throws Exception {
-//        Map conf = new HashMap();
-//        //topology所有自定义的配置均放入这个Map
-//        TopologyBuilder builder = new TopologyBuilder();
-//        //创建topology的生成器
-//        int spoutParal = 1;
-//        //获取spout的并发设置
-//        SpoutDeclarer spout = builder.setSpout("out-word", new WordSpout(), spoutParal);
-//        //创建Spout， 其中new SequenceSpout() 为真正spout对象，SequenceTopologyDef.SEQUENCE_SPOUT_NAME 为spout的名字，注意名字中不要含有空格
-//        int boltParal = 1;
-//        //获取bolt的并发设置
-//        BoltDeclarer totalBolt = builder.setBolt("word-count", new CountBolt(), boltParal).shuffleGrouping("out-word");
-//        //创建bolt， SequenceTopologyDef.TOTAL_BOLT_NAME 为bolt名字，TotalCount 为bolt对象，boltParal为bolt并发数，
-//        // shuffleGrouping（SequenceTopologyDef.SEQUENCE_SPOUT_NAME），
-//        // 表示接收SequenceTopologyDef.SEQUENCE_SPOUT_NAME的数据，并且以shuffle方式，
-//        // 即每个spout随机轮询发送tuple到下一级bolt中
-//        int ackerParal = 1;
-//        Config.setNumAckers(conf, ackerParal);
-//        //设置表示acker的并发数
-//        int workerNum = 2;
-//        conf.put(Config.TOPOLOGY_WORKERS, workerNum);
-//        //表示整个topology将使用几个worker
-//        conf.put(Config.STORM_CLUSTER_MODE, "distributed");
-//        //设置topolog模式为分布式，这样topology就可以放到JStorm集群上运行
-//        StormSubmitter.submitTopology("streamName", conf, builder.createTopology());
-//        //提交topology
+        if (local) {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology(TOPOLOGY_NAME, conf, tridentTopology.build());
+        } else {
+            StormSubmitter.setLocalNimbus(null);
+            StormSubmitter.submitTopology(TOPOLOGY_NAME, conf, tridentTopology.build());
+        }
     }
 }
